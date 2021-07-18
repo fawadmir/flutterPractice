dcb1fe292e63531820def50c4a2c538deac293ea
fawadmir <fawadmir25@gmail.com>
2021-07-17 22:17:48 -0700

    Day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class HomePage extends StatelessWidget {
+  final int days=30;
+  final String name="Mir";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar: AppBar(
+    title: Center(child: Text("Catalog"),),
+    ),  
+    body: Center(
+    child: Container(
+    child: Text("Welcome to $days days to flutter $name"),),),
+    drawer: Drawer(),        
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home_page.dart';
  
 void main() {
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
-  const MyApp({ Key key }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-          home:Material(
-            child: Container(
-            child: Center(
-            child: Text("Welcome to 30 days to flutter")),
-            ),
-          ),
-    );
+      home: HomePage()
+      );
   }
 }
