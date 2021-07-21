f40c3ee08827dc012c7a1eec0838cc81ff7845b4
fawadmir <fawadmir25@gmail.com>
2021-07-20 23:31:21 -0700

    Day 3 completed copy

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(
       brightness: Brightness.dark,  
       ),
+     
       routes: {
         "/login":(context)=>LoginPage(),
       },
