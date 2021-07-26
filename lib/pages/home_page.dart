e9ff106083317c03baf03f520e51a72493698e96
fawadmir <fawadmir25@gmail.com>
2021-07-25 23:31:53 -0700

    day 13 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,38 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 import 'package:flutter_catalog/widgets/item_widget.dart';
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+class _HomePageState extends State<HomePage> {
   final int days=30;
   final String name="Mir";
+  
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+
+  loadData() async{
+   await Future.delayed(Duration(seconds: 2));
+   final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+   final decodedData= jsonDecode(catalogJson);
+   final productsData=decodedData["products"];
+   CatalogModel.items=List.from(productsData)
+   .map<Item>((item) => Item.fromMap(item))
+   .toList();
+   setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    final dumyList=List.generate(5, (index) => CatalogModel.items[0]);
+    //final dumyList=List.generate(5, (index) => CatalogModel.items[0]);
     return Scaffold(
     appBar: AppBar(
     title: Center(
@@ -16,17 +41,18 @@ class HomePage extends StatelessWidget {
     ),  
     body:Padding(
     padding: const EdgeInsets.all(16.0),
-    child: ListView.builder(
-    //itemCount: CatalogModel.items.length,
-    itemCount: dumyList.length,
+    child: (CatalogModel.items !=null && CatalogModel.items.isNotEmpty)?
+    ListView.builder(
+    itemCount: CatalogModel.items.length,
+    //itemCount: dumyList.length,
 
     itemBuilder: (context, index){
     return ItemWidget(
-    //item: CatalogModel.items[index],
-    item:dumyList[index],
+    item: CatalogModel.items[index],
+    //item:dumyList[index],
     );  
     },
-    ),
+    ): Center(child: CircularProgressIndicator()),
     ),
     drawer: MyDrawer(),        
     );
